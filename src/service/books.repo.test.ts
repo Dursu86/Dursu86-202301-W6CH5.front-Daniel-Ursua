28044482335cbd85a3491882cdb68793db30cc9e
Daniel <dursuaf@gmail.com>
2023-02-26 20:36:40 +0100

    Add repo and test

---
+++ src/service/books.repo.test.ts
@@ -0,0 +1,130 @@
+import { BookStructure } from "../models/book";
+import { BooksRepo } from "./books.repo";
+
+const repo = new BooksRepo();
+
+describe("Given the books repo", () => {
+  // OK tests
+
+  describe("When create a new instance of the class and call method readAll", () => {
+    test("Then it should create the item and return the values fetched", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest
+          .fn()
+          .mockResolvedValue([{ test: "test" }] as unknown as BookStructure[]),
+      });
+
+      expect(repo).toBeInstanceOf(BooksRepo);
+      const readAll = await repo.readAll();
+      expect(readAll).toEqual([{ test: "test" }]);
+    });
+  });
+
+  describe("When it calls the method readOne", () => {
+    test("Then it should return the value of the one asked", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest
+          .fn()
+          .mockResolvedValue({ id: 2, test: "test2" } as unknown as Scrub),
+      });
+
+      const readOne = await repo.readOne(2);
+      expect(readOne).toEqual({ id: 2, test: "test2" });
+    });
+  });
+
+  describe("When it calls the method update", () => {
+    test("Then it should return the updated value", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: 2,
+          test: "test3",
+        } as unknown as BookStructure),
+      });
+
+      const update = await repo.update({
+        test: "test3",
+      } as unknown as Partial<BookStructure>);
+      expect(update).toEqual({ id: 2, test: "test3" });
+    });
+  });
+
+  describe("When it calls the method create", () => {
+    test("Then it should return the value created", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: 3,
+          test: "test4",
+        } as unknown as BookStructure),
+      });
+
+      const create = await repo.create({
+        test: "test4",
+      } as unknown as Partial<BookStructure>);
+      expect(create).toEqual({ id: 3, test: "test4" });
+    });
+  });
+
+  describe("When it calls the method delete", () => {
+    test("Then it should call fetch with no return", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn(),
+      });
+
+      const mockDelete = await repo.delete(2);
+      expect(fetch).toHaveBeenCalled();
+      expect(mockDelete).toBe(undefined);
+    });
+  });
+
+  // Error tests
+
+  describe("When readAll method fails to fetch", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const readAll = repo.readAll();
+      await expect(readAll).rejects.toThrow();
+    });
+  });
+
+  describe("When readOne method fails to fetch", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const readOne = repo.readOne(1);
+      await expect(readOne).rejects.toThrow();
+    });
+  });
+
+  describe("When update method fails to fetch", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const update = repo.update({
+        test: "test",
+      } as unknown as Partial<BookStructure>);
+      await expect(update).rejects.toThrow();
+    });
+  });
+
+  describe("When create method fails to fetch", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const create = repo.create({
+        test: "test",
+      } as unknown as Partial<BookStructure>);
+      await expect(create).rejects.toThrow();
+    });
+  });
+
+  describe("When delete method fails to fetch", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const mockDelete = repo.delete(1);
+      await expect(mockDelete).rejects.toThrow();
+    });
+  });
+});
---
+++ src/service/books.repo.ts
@@ -0,0 +1,70 @@
+import { BookStructure } from "../models/book";
+
+export interface BooksRepoStructure {
+  readAll(): Promise<BookStructure[]>;
+  readOne(id: BookStructure["id"]): Promise<BookStructure>;
+  create(info: Partial<BookStructure>): Promise<BookStructure>;
+  update(info: Partial<BookStructure>): Promise<BookStructure>;
+  delete(id: BookStructure["id"]): Promise<void>;
+}
+
+export class BooksRepo implements BooksRepoStructure {
+  constructor(public url: string = "http://localhost:4500/books") {}
+
+  async readAll(): Promise<BookStructure[]> {
+    const resp = await fetch(this.url);
+    if (!resp.ok)
+      throw new Error("Error HTTP " + resp.status + ". " + resp.statusText);
+    const BookStructure = (await resp.json()) as BookStructure[];
+    return BookStructure;
+  }
+
+  async readOne(id: number): Promise<BookStructure> {
+    const url = this.url + "/" + id;
+    const resp = await fetch(url);
+    if (!resp.ok)
+      throw new Error("Error HTTP " + resp.status + ". " + resp.statusText);
+
+    const BookStructure = (await resp.json()) as BookStructure;
+    return BookStructure;
+  }
+
+  async update(info: Partial<BookStructure>): Promise<BookStructure> {
+    const url = this.url + "/" + info.id;
+    const resp = await fetch(url, {
+      method: "PATCH",
+      body: JSON.stringify(info),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    if (!resp.ok)
+      throw new Error("Error HTTP " + resp.status + ". " + resp.statusText);
+    const data = (await resp.json()) as BookStructure;
+    return data;
+  }
+
+  async create(info: Partial<BookStructure>): Promise<BookStructure> {
+    const resp = await fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(info),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    if (!resp.ok)
+      throw new Error("Error HTTP " + resp.status + ". " + resp.statusText);
+
+    const data = (await resp.json()) as BookStructure;
+    return data;
+  }
+
+  async delete(id: number): Promise<void> {
+    const url = this.url + "/" + id;
+    const resp = await fetch(url, {
+      method: "DELETE",
+    });
+    if (!resp.ok)
+      throw new Error("Error HTTP " + resp.status + ". " + resp.statusText);
+  }
+}
