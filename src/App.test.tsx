201e80013366c15eb3e0872536ea420ef067d1ea
Daniel <dursuaf@gmail.com>
2023-02-27 08:51:14 +0100

    Fix importation problem

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
+import { App } from "./App";
 
 describe("Given the App component", () => {
   describe("When it is render", () => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 
 export function App() {
   return <h1>Test</h1>;
